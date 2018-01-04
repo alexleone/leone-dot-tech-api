701e0a8f0ca5c4a48501f1c3fc5e7aa4f59600d8
Alex Leone <alex@leone.tech>
2018-01-04 12:17:14 -0500

    Added the missing authorizer on the list endpoint and test script

---
+++ tests/note-list.sh
@@ -0,0 +1,11 @@
+ apig-test \
+--username='admin@example.com' \
+--password='Passw0rd!' \
+--user-pool-id='us-east-1_ql58aKgir' \
+--app-client-id='5d1b1pop2i78qid6c6j83thgto' \
+--cognito-region='us-east-1' \
+--identity-pool-id='us-east-1:2be15265-4b74-4772-a146-bc4e32c6464a' \
+--invoke-url='https://llmjysubbc.execute-api.us-east-1.amazonaws.com/prod' \
+--api-gateway-region='us-east-1' \
+--path-template='/notes' \
+--method='GET'
